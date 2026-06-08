093f0538f6acd1704aa48400965e0240c73ebc57
Jaime <jaimebrs1979@gmail.com>
2026-06-08 13:11:48 -0500

    add config typeorm en nest + migrations

---
+++ src/database/migrations/1780938983892-create_prueba_tabla.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreatePruebaTabla1780938983892 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
